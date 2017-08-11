f4415558fd9cceb3cdf9c4cb86771805c3ec3faa
Azuwir Azzurri <azuwir@gmail.com>
2017-08-11 13:21:17 +0700

    Dashboard: Summary kasus dan tahanan

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Kasus;
+use App\Subyek;
+use App\KategoriSubyek;
 
 class HomeController extends Controller
 {
@@ -23,7 +26,20 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $dashboard = array();
+        
+        $kasus_rp2 = Kasus::where('status_rp2','<>',0)->count();
+        $kasus_rp3mum = Kasus::where('status_rp3mum','<>',0)->count();
+        $kasus_rp3sus = Kasus::where('status_rp3sus','<>',0)->count();
+        $tahanan = Subyek::where('status','<>',0)->count();
+        $subyek_hukum = KategoriSubyek::select('name')->get()->toArray();
+
+        $dashboard["kasus_rp2"] = $kasus_rp2;
+        $dashboard["kasus_rp3mum"] = $kasus_rp3mum;
+        $dashboard["kasus_rp3sus"] = $kasus_rp3sus;
+        $dashboard["tahanan"] = $tahanan;
+        
+        return view('home', ['dashboard' => $dashboard, 'subyek_hukum' => json_encode($subyek_hukum)]);
     }
 
     public function trial()
