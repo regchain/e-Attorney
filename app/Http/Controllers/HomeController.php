e65801ce585607fdf681d5ea9d1d5310c07b09cf
Azuwir Azzurri <azuwir@gmail.com>
2017-09-19 06:35:35 +0700

    Clean Code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,23 +30,30 @@ class HomeController extends Controller
     {
         $dashboard = array();
         
-        $kasus_rp2 = Kasus::where('status_rp2','<>',0)
+        $kasus_rp2 = Kasus::where('status_rp2',1)
             ->where('status_rp3mum', 0)
             ->count();
 
-        $kasus_rp3mum = Kasus::where('status_rp3mum','<>',0)
+        $kasus_rp3mum = Kasus::where('status_rp3mum',1)
             ->where('status_rp3sus', 0)
             ->count();
 
         $kasus_rp3sus = Spt::join('kasus','spt.kasus_id','=','kasus.id')
             ->join('surats','spt.surat_id','=','surats.id')
             ->where('surats.tipe_surat','=','RP3SUS')
-            ->where('kasus.status_rp3sus','<>',0)->count();
+            ->where('kasus.status_rp3sus',1)->count();
 
         $tahanan = Subyek::where('status','<>',0)->count();
         
         $subyek_hukum = KategoriSubyek::selectRaw('name, count(kategori_subyek_id) as total')
             ->join('subyek','kategori_subyeks.id','=','subyek.kategori_subyek_id')
+            ->where('subyek.status','<>',0)
+            ->groupBy('kategori_subyeks.name')
+            ->get();
+
+        $obyek_pidana = KategoriSubyek::selectRaw('name, count(kategori_subyek_id) as total')
+            ->join('subyek','kategori_subyeks.id','=','subyek.kategori_subyek_id')
+            ->where('subyek.status','<>',0)
             ->groupBy('kategori_subyeks.name')
             ->get();
         
--- routes/web.php
+++ routes/web.php
@@ -27,53 +27,6 @@ Route::resource('pengumuman', 'PengumumanController');
 
 Route::get('penyidikan', 'LayoutController@penyidikan');
 
-/*
-|--------------------------------------------------------------------------
-| Obyek [Benda Sitaan] Routes
-|--------------------------------------------------------------------------
-|
-| frb1 = surat perintah geledah
-| erb1 = surad edit benda sitaan [Berita Acara Penyitaan]
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('geledah', 'ObyekController@geledah');
-
-Route::get('erb1', 'ObyekController@erb1');
-
-/*
-|--------------------------------------------------------------------------
-| RP-3sus Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('rp3sus', 'Rp3susController@index');
-
-Route::get('frp3sus', 'Rp3susController@frp3sus');
-
-/*
-|--------------------------------------------------------------------------
-Subyek [Tersangka] Routes
-|--------------------------------------------------------------------------
-|
-| frt1 = Surat Penetapan Tersangka
-| frt2 = surat perintah penahanan
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('frt0', 'SubyekController@frt0');
-
-Route::get('frt2', 'SubyekController@frt2');
-
-Route::get('ert1', 'SubyekController@ert1');
-
 Route::group(['prefix' => '', 'middleware' => ['auth']], function() {
 	Route::resource('jaksa', 'JaksasController');
 	Route::resource('pasal', 'PasalsController');
